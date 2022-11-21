9c7b7908acd4756811bf658ed0e8e51745f342ad
nazarKuznetsov <cuzyalol@gmail.com>
2022-11-21 16:38:06 +0200

    Add general colors to colorset

---
+++ NoNameNetwork/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  NoNameNetwork
+//
+//  Created by Admin on 20.11.2022.
+//
+
+import Foundation
---
+++ NoNameNetwork/Extensions/ExtensionForViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ExtensionForViewController.swift
+//  NoNameNetwork
+//
+//  Created by Admin on 20.11.2022.
+//
+
+import Foundation
